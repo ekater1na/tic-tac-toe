94476cda2a12df2ac788be18bc30b0013bc91074
ekater1na <ekaterina_l@tut.by>
2019-12-09 16:19:43 +0300

    Updates

---
+++ precache-manifest.a01daf3890568bf1f29f0a41b494853d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bc285db20ced3f62ca1005dd52ea1e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3b095268c7cdc2a7f05f",
+    "url": "/tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "af478b6ac43632db1334",
+    "url": "/tic-tac-toe/static/js/2.dd77b70f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.dd77b70f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b095268c7cdc2a7f05f",
+    "url": "/tic-tac-toe/static/js/main.0217f92e.chunk.js"
+  },
+  {
+    "revision": "e5d7aca4491c82ee1a95",
+    "url": "/tic-tac-toe/static/js/runtime-main.33bcfadf.js"
+  }
+]);
